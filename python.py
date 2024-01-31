3932ad6720182602cdc0aa1afe7e157bcda92578
Tobit56 <158276161+Tobit56@users.noreply.github.com>
2024-01-31 10:51:30 +0000

    l

--- python.py
+++ python.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+import random
+
 app = Flask(__name__)
 
 @app.route("/")
 def article():
-    return render_template("html.html")
+    cislo = random.randint(1, 10)
+    return render_template("html.html", cislo)
+    
